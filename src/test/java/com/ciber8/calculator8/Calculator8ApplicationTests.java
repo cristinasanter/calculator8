bf4acf236ecada5a6537ea215d70727ec6223410
cristinasanter <cristinasancar00@gmail.com>
2022-04-26 17:32:34 +0000

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber8/calculator8/Calculator8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber8.calculator8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
