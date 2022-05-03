1f602c9c60f34d422ef11bacbfe727f8baa42603
cristinasanter <cristinasancar00@gmail.com>
2022-05-03 16:08:37 +0000

    Todos los tab eliminados

--- src/main/java/com/ciber8/calculator8/Calculator8Application.java
+++ src/main/java/com/ciber8/calculator8/Calculator8Application.java
@@ -1,8 +1,13 @@
 package com.ciber8.calculator8;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Calculator8Application {
+
 public static void main(String[] args) {
 SpringApplication.run(Calculator8Application.class, args);
-}}
+}
+
+}
