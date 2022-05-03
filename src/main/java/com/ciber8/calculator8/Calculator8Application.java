3a8df6af2ad876523c1dfbe384dc448ddeced109
cristinasanter <cristinasancar00@gmail.com>
2022-05-03 15:59:23 +0000

    Prueba tab eliminados

--- src/main/java/com/ciber8/calculator8/Calculator8Application.java
+++ src/main/java/com/ciber8/calculator8/Calculator8Application.java
@@ -1,13 +1,8 @@
 package com.ciber8.calculator8;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Calculator8Application {
-
 public static void main(String[] args) {
 SpringApplication.run(Calculator8Application.class, args);
-}
-
-}
+}}
